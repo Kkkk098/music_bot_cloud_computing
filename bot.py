5278b0f6af8d0edf57cbb92c1db3bd93a80859ba
Kkkk098 <193816090+Kkkk098@users.noreply.github.com>
2026-03-06 04:28:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -204,7 +204,7 @@ def main():
             pass
 
     # app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
-    app = ApplicationBuilder().token(TOKEN).httpx_timeout(Timeout(60.0, read=120.0)).build()
+    app = ApplicationBuilder().token(TELEGRAM_TOKEN).httpx_timeout(Timeout(60.0, read=120.0)).build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("help", help_command))
