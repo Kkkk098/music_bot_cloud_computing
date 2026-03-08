6d9e3b26b2ca2f450305544e513f490e6d39e57e
Kkkk098 <193816090+Kkkk098@users.noreply.github.com>
2026-03-08 19:46:51 +0300

    Create provider.tf

---
+++ terraform_yc/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "~> 0.111"
+    }
+  }
+}
+
+provider "yandex" {
+  service_account_key_file = file("key.json")
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = "ru-central1-a"
+}
